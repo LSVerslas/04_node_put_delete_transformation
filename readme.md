004ca337f42d31f3aed1411f0c792a47d655edd1
Sakalis <lsverslas@gmail.com>
2023-12-18 09:03:31 +0200

    Padaryta

---
+++ readme.md
@@ -0,0 +1,5 @@
+pradeti nauja projekta
+git + github
+npm
+express, morgan, cors, nodemon(dev)
+pasirasyti basic express server kuris suksis and port 3000
--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
 
